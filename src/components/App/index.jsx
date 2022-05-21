6ef07d01b392392b16570800c8fd4c811d04bb25
Nikola Kabátová <niki.kabatova@gmail.com>
2022-05-21 11:51:45 +0200

    handleSubmit function

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservation" element={<Reservation />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
     <Footer />
-  </>
+  </BrowserRouter>
 );
